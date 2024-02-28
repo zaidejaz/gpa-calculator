5a3dc6dfbd5221d0ff433da046a2b8a2cf4831f4
zaidejaz <zaidejaz521@gmail.com>
2024-02-28 15:25:58 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,139 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from "react";
 
-export default function Home() {
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+type Subject = {
+  grade: string;
+  credit: number | undefined;
+};
+
+const CalculateGPA: React.FC = () => {
+  const [subjects, setSubjects] = useState<Subject[]>([
+    {
+      grade: "",
+      credit: undefined,
+    },
+    {
+      grade: "",
+      credit: undefined,
+    },
+  ]);
+
+  const [gpa, setGpa] = useState<number>(0);
+
+  const handleGradeChange = (index: number, value: string) => {
+    setSubjects((prevSubjects) =>
+      prevSubjects.map((subject, i) =>
+        i === index ? { ...subject, grade: value } : subject
+      )
+    );
+  };
+
+  const handleCreditChange = (index: number, value: number) => {
+    setSubjects((prevSubject) =>
+      prevSubject.map((subject, i) =>
+        i === index ? { ...subject, credit: value } : subject
+      )
+    );
+  };
+
+  const calculateGPA = () => {
+    let totalGradePoints = 0;
+    let totalCreditHours = 0;
+
+    subjects.forEach((subject) => {
+      let gradePoint: number;
+
+      switch (subject.grade.toUpperCase()) {
+        case "A":
+          gradePoint = 4.0;
+          break;
+        case "B+":
+          gradePoint = 3.5;
+          break;
+        case "B":
+          gradePoint = 3.0;
+          break;
+        case "C+":
+          gradePoint = 2.5;
+          break;
+        case "C":
+          gradePoint = 2.0;
+          break;
+        case "D+":
+          gradePoint = 1.5;
+          break;
+        case "D":
+          gradePoint = 1.0;
+          break;
+        case "F":
+          gradePoint = 0.0;
+          break;
+        default:
+          console.log("Invalid Grade Entered");
+          return 0;
+      }
+
+      if (subject.credit !== undefined) {
+        totalGradePoints += gradePoint * subject.credit;
+        totalCreditHours += subject.credit;
+      }
+    });
+
+    if (totalCreditHours === 0) {
+      console.log("No credit hours entered.");
+      return;
+    }
+
+    const cgpa = totalGradePoints / totalCreditHours;
+    setGpa(cgpa);
+  };
+
+  const addField = () => {
+    const subject = {
+      grade: "",
+      credit: undefined,
+    };
+    setSubjects([...subjects, subject]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className=" max-w-4xl mx-auto p-2  md:p-10 rounded-xl">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mt-4">
+        Calculate GPA
+      </h1>
+      <p className="leading-7 mb-4">According to the standards of International Islamic University Islamabad</p>
+      {subjects.map((subject, index) => (
+        <div className="flex md:items-center" key={index}>
+          <p className="px-2 md:px-4 mt-4 md:mt-0">{index + 1}.</p>
+          <div className="flex flex-col md:flex-row md:gap-4">
+            <Input
+              type="text"
+              value={subject.grade}
+              onChange={(e) => handleGradeChange(index, e.target.value)}
+              placeholder="Enter Grade"
             />
-          </a>
+            <Input
+              type="number"
+              value={subject.credit}
+              onChange={(e) =>
+                handleCreditChange(index, parseInt(e.target.value))
+              }
+              placeholder="Enter Credit Hours"
+            />
+          </div>
         </div>
+      ))}
+      <div className="text-center space-y-2 md:my-0 m-4 space-x-4">
+        <Button onClick={addField}>Add another subject</Button>
+        <Button onClick={() => calculateGPA()}>Calculate CGPA</Button>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {gpa > 0 ? <p className="text-center leading-7 [&:not(:first-child)]:mt-6">Your GPA is {gpa.toFixed(2)}</p> : <p></p>}
+    </div>
   );
-}
+};
+
+export default CalculateGPA;
